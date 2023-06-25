0a30dd158f7ca79c18b6819b3b12a842ee27452f
ironcladgeek <ironcladgeek@hotmail.com>
2023-06-25 21:22:28 +0000

    Write comments for utils.encode()

--- src/app/utils.py
+++ src/app/utils.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+
 def encode(sentence, dim=500):
+    """This function mimics encoding a sentence into a n-dimensional embedding.
+    In a real case scenario encoding could be done using something like 
+    sentence_transformers: https://www.sbert.net/index.html
+    """
     return np.random.rand(dim).tolist()
-    
