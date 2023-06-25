d8d3afc5ff4ec895a4294e7ca06428a39c273993
ironcladgeek <ironcladgeek@hotmail.com>
2023-06-25 21:13:25 +0000

    Create API endpoint for encoding a sentence

--- src/app/api.py
+++ src/app/api.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
+import utils
 
 router = APIRouter()
+
+
+@router.post("/encode")
+def encode_sentence(sentence: str):
+    return utils.encode(sentence=sentence, dim=500)
---
+++ src/app/utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def encode(sentence, dim=500):
+    return np.random.rand(dim).tolist()
+    
