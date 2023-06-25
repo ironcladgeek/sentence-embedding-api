4cf253bd23c948984af5251f41fb88a005680191
ironcladgeek <ironcladgeek@hotmail.com>
2023-06-25 20:53:31 +0000

    Basic setup for application

---
+++ src/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from loguru import logger
+
+from app import api
+from app.config import settings, setup_app_logging
+
+# setup logging as early as possible
+setup_app_logging(config=settings)
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+)
+
+app.include_router(api.router, prefix=settings.API_V1_STR)
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    logger.warning("Running in development mode. Do not run like this in production.")
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8001, log_level="debug")
